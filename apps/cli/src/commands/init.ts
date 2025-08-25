a12d787bf2aef2bc637768b67d73dc93317fe3ed
Pranoy Majumdar <works.pranoy@gmail.com>
2025-08-25 20:49:04 +0530

    refactor(cli): remove unnecessary try-catch block

--- apps/cli/src/commands/init.ts
+++ apps/cli/src/commands/init.ts
@@ -5,18 +5,11 @@ import { getAnswers } from "../utils/getAnswers";
 export async function init() {
   p.intro("🚀 Welcome to Hypr Stack! Let's scaffold your project.");
 
-  try {
-    const answers = await getAnswers();
-    console.log(answers);
-    const projectCreation = await createProject(answers);
-
-    if (!projectCreation.success) {
-      p.cancel(`❌ Failed to create project: ${projectCreation.error}`);
-      return;
-    }
-
-    p.outro("✅ Your Hypr Stack project is ready to go!");
-  } catch (err) {
-    p.cancel(`Unexpected error: ${(err as Error).message}`);
+  const answers = await getAnswers();
+  const projectCreation = await createProject(answers);
+  if (!projectCreation.success) {
+    return;
   }
+
+  p.outro("✅ Your Hypr Stack project is ready to go!");
 }
